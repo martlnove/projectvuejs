3db85e71fa9644b7745db7825917b28ee4aa7e39
Lucas Martins <lucasshc@gmail.com>
2021-11-16 21:07:31 -0300

    Criação do botão de remover foto via api

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,48 +1,52 @@
+<!-- alurapic/src/components/home/Home.vue -->
+
 <template>
-  <div>
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título">
+    <div>    
+        <h1 class="centralizado">Alurapic</h1>
 
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <!-- novo elemento para exibir mensagens para o usuário -->
+        <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
 
-        <meu-painel :titulo="foto.titulo">
-          
-          <imagem-responsiva v-meu-transform:scale.animate="15" :url="foto.url" :titulo="foto.titulo"/>
-          <meu-botao 
-            tipo="button" 
-            rotulo="REMOVER" 
-            @botaoAtivado="remove(foto)"
-            :confirmacao="true"
-            estilo="perigo"/>
-          
-        </meu-painel>
-
-      </li>
-    </ul>
-  </div>
+        <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+        <ul class="lista-fotos">
+          <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+              <meu-painel :titulo="foto.titulo">
+                <imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform:scale.animate="1.2"/>
+                <meu-botao 
+                  rotulo="Remover" 
+                  tipo="button" 
+                  estilo="perigo"
+                  :confirmacao="true" 
+                  @botaoAtivado="remove(foto)"/>
+              </meu-painel>
+          </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+
 import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
 import Botao from '../shared/botao/Botao.vue';
 
 export default {
 
   components: {
-    'meu-painel' : Painel, 
-    'imagem-responsiva': ImagemResponsiva,
-    'meu-botao' : Botao
+
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
   },
 
-  data() {
-
+  data () {
     return {
 
-      titulo: 'Alurapic', 
-      fotos: [], 
-      filtro: ''
+      fotos: [],
+
+      filtro: '',
+
+      mensagem: ''
     }
   },
 
@@ -50,37 +54,46 @@ export default {
 
     fotosComFiltro() {
 
-      if(this.filtro) {
+      if (this.filtro) {
         let exp = new RegExp(this.filtro.trim(), 'i');
         return this.fotos.filter(foto => exp.test(foto.titulo));
       } else {
         return this.fotos;
       }
     }
+
   },
 
   methods: {
 
-    remove(foto) { 
-        alert('Remover a foto!' + foto.titulo);
+    remove(foto) {
+      this.$http
+        .delete(`http://localhost:3000/v1/fotos/${foto._id}`)
+        .then(() => {
+            // assim que apagar, exibe a mensagem para o usuário
+            this.mensagem = 'Foto removida com sucesso'
+          }, 
+          err => {
+            this.mensagem = 'Não foi possível remover a foto';
+            console.log(err);
+          }
+        )
     }
 
   },
 
   created() {
 
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http
+      .get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
-
 </script>
-
 <style>
 
   .centralizado {
-
     text-align: center;
   }
 
@@ -89,12 +102,10 @@ export default {
   }
 
   .lista-fotos .lista-fotos-item {
-
     display: inline-block;
   }
 
   .filtro {
-
     display: block;
     width: 100%;
   }
