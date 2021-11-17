1f35052dea47328fbcc40e4e0e2a1b4eaafb6f1d
Lucas Martins <lucasshc@gmail.com>
2021-11-16 21:07:59 -0300

    Update Home.vue

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,112 +1,110 @@
-<!-- alurapic/src/components/home/Home.vue -->
-
 <template>
-    <div>    
-        <h1 class="centralizado">Alurapic</h1>
+  <div>
+    <h1 class="centralizado">Alurapic</h1>
 
-        <!-- novo elemento para exibir mensagens para o usuário -->
-        <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
 
-        <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-        <ul class="lista-fotos">
-          <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-              <meu-painel :titulo="foto.titulo">
-                <imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform:scale.animate="1.2"/>
-                <meu-botao 
-                  rotulo="Remover" 
-                  tipo="button" 
-                  estilo="perigo"
-                  :confirmacao="true" 
-                  @botaoAtivado="remove(foto)"/>
-              </meu-painel>
-          </li>
-        </ul>
-    </div>
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="filtre pelo título da foto"
+    />
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva
+            :url="foto.url"
+            :titulo="foto.titulo"
+            v-meu-transform:scale.animate="1.2"
+          />
+          <meu-botao
+            rotulo="Remover"
+            tipo="button"
+            estilo="perigo"
+            :confirmacao="true"
+            @botaoAtivado="remove(foto)"
+          />
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-
-import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
-import Botao from '../shared/botao/Botao.vue';
+import Painel from "../shared/painel/Painel.vue";
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
-
   components: {
-
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva, 
-    'meu-botao': Botao
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
   },
 
-  data () {
+  data() {
     return {
-
       fotos: [],
 
-      filtro: '',
+      filtro: "",
 
-      mensagem: ''
-    }
+      mensagem: "",
+    };
   },
 
   computed: {
-
     fotosComFiltro() {
-
       if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter((foto) => exp.test(foto.titulo));
       } else {
         return this.fotos;
       }
-    }
-
+    },
   },
 
   methods: {
-
     remove(foto) {
-      this.$http
-        .delete(`http://localhost:3000/v1/fotos/${foto._id}`)
-        .then(() => {
-            // assim que apagar, exibe a mensagem para o usuário
-            this.mensagem = 'Foto removida com sucesso'
-          }, 
-          err => {
-            this.mensagem = 'Não foi possível remover a foto';
-            console.log(err);
-          }
-        )
-    }
-
+      this.$http.delete(`http://localhost:3000/v1/fotos/${foto._id}`).then(
+        () => {
+          // assim que apagar, exibe a mensagem para o usuário
+          this.mensagem = "Foto removida com sucesso";
+        },
+        (err) => {
+          this.mensagem = "Não foi possível remover a foto";
+          console.log(err);
+        }
+      );
+    },
   },
 
   created() {
-
     this.$http
-      .get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
-}
+      .get("http://localhost:3000/v1/fotos")
+      .then((res) => res.json())
+      .then(
+        (fotos) => (this.fotos = fotos),
+        (err) => console.log(err)
+      );
+  },
+};
 </script>
 <style>
+.centralizado {
+  text-align: center;
+}
 
-  .centralizado {
-    text-align: center;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
-  }
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
