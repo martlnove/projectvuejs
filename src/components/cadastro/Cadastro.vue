7b05b7e1120d2403ea069be1e7f629376bd19a35
Lucas Martins <lucasshc@gmail.com>
2021-12-21 23:13:39 -0300

    Update Cadastro.vue

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -10,14 +10,14 @@
 
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input name="titulo" v-validate data-vv-rules="required" id="titulo" autocomplete="off" v-model="foto.titulo">
-        <span v-show="errors.has('titulo')">ERRO</span>
+        <input data-vv-as="título" name="titulo" v-validate data-vv-rules="required|min:3|max:30" id="titulo" autocomplete="off" v-model="foto.titulo">
+        <span class="erro" v-show="errors.has('titulo')">{{errors.first('titulo')}}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
         <input name= "url" v-validate data-vv-rules="required" id="url" autocomplete="off" v-model="foto.url">
-        <span v-show="errors.has('url')">ERRO</span>
+        <span class="erro" v-show="errors.has('url')">{{erros.first('url')}}</span>
         <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
@@ -64,15 +64,22 @@ export default {
 
       grava() {
 
-          this.service
-            .cadastra(this.foto)
-            .then(() => {
-              if(this.id) this.$router.push({name: 'home'});
-              this.foto = new Foto();},
-              err => console.log(err));
-      }
-  },
+        this.$validator
+          .validateAll()
+          .then(success => {
+            if(success) {
 
+              this.service
+                .cadastra(this.foto)
+                .then(() => {
+                  if(this.id) this.$router.push({ name: 'home'});
+                  this.foto = new Foto()
+                }, 
+                err => console.log(err));
+            }
+        });
+    }
+  },
   created() {
 
     this.service = new FotoService(this.$resource);
@@ -109,5 +116,9 @@ export default {
 
   .centralizado {
     text-align: center;
+  }
+
+  .erro {
+    color: red;
   }
 </style>
